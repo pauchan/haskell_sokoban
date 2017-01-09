a98c90ee6babdca185058164f3e2e40202dde4e7
Pawel Rusin <rusin.paw@gmail.com>
2017-01-09 22:37:57 +0900

    first function definitions

--- src/Main.hs
+++ src/Main.hs
@@ -2,4 +2,19 @@ module Main where
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+	args <- getArgs
+	handle <- openFile (head args) ReadMode
+	contents <- hGetContents handle
+	putStr contents
+	hClose handle
+
+-- the (Int, Int) tuple as a second parameter denotes
+-- (horizontal/vertical orientation, back or forward)
+move :: (Int, Int) -> (Int, Int) -> (Int, Int)
+move = undefined
+
+decodeUserInput :: Char -> (Int, Int)
+decodeUserInput = undefined
+
+applyMoveToMap :: [[Char]] -> (Int, Int) -> [[Char]]
+applyMoveToMap = undefined
