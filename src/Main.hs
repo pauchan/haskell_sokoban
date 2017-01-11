25da94e461b4efef3b706b4e89143886d5f22226
Pawel Rusin <rusin.paw@gmail.com>
2017-01-11 22:13:25 +0900

    make project compilable

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,33 @@
 module Main where
 
+import System.Environment
+import System.IO
+import Data.List
+
+readMap :: String -> [[Char]] -> [[Char]]
+readMap x y = x : y
+
 main :: IO ()
 main = do
 	args <- getArgs
 	handle <- openFile (head args) ReadMode
 	contents <- hGetContents handle
-	putStr contents
+	let map = readMap contents []
+	putStr (head map)
 	hClose handle
 
+applyMoveToMap :: [[Char]] -> (Int, Int) -> [[Char]]
+applyMoveToMap = undefined
+
 -- the (Int, Int) tuple as a second parameter denotes
--- (horizontal/vertical orientation, back or forward)
+-- (horizontal/vertical orientation (0/1), back or forward(-1/1))
 move :: (Int, Int) -> (Int, Int) -> (Int, Int)
 move = undefined
 
 decodeUserInput :: Char -> (Int, Int)
-decodeUserInput = undefined
-
-applyMoveToMap :: [[Char]] -> (Int, Int) -> [[Char]]
-applyMoveToMap = undefined
+decodeUserInput x = case x of
+	'w' -> (1,-1)
+	'a' -> (0,-1)
+	's' -> (1,1)
+	'd' -> (0,1)
+	_   -> (0,0)
